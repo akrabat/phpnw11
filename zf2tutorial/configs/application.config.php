2240940157c40882f4b176e9c71dc97dd1977168
Rob Allen <rob@akrabat.com>
2011-09-27 19:59:51 +0100

    ZF2 prototype

---
+++ zf2tutorial/configs/application.config.php
@@ -0,0 +1,16 @@
+<?php
+return new Zend\Config\Config(array(
+    'module_paths' => array(
+        realpath(__DIR__ . '/../modules'),
+    ),
+    'modules' => array(
+        'ZendModule', // paradox?!
+        'ZendMvc',
+       // 'Album',
+        'Application', // load last
+    ),
+    'module_config' => array( 
+        'cache_config'  => false,
+        'cache_dir'     => realpath(__DIR__ . '/../data/cache'),
+    ),
+));
