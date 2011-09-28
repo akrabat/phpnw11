89848229d27e81bd2864b522a14be23a48d88583
Rob Allen <rob@akrabat.com>
2011-09-28 22:06:50 +0100

    make add album work

--- zf2tutorial/modules/Album/Controller/AlbumController.php
+++ zf2tutorial/modules/Album/Controller/AlbumController.php
@@ -4,7 +4,8 @@ namespace Album\Controller;
 
 use Zend\Mvc\Controller\ActionController,
     Zend\Mvc\Router\RouteStack,
-    Album\Model\DbTable\Albums;
+    Album\Model\DbTable\Albums,
+    Album\Form\Album as AlbumForm;
 
 class AlbumController extends ActionController
 {
@@ -21,6 +22,35 @@ class AlbumController extends ActionController
             'content' => '',
             'albums' => $this->albums->fetchAll(),
         );
+    }
+
+    public function addAction()
+    {
+        $form = new AlbumForm();
+        $form->submit->setLabel('Add');
+
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $formData = $request->post()->toArray();
+            if ($form->isValid($formData)) {
+                $artist = $form->getValue('artist');
+                $title = $form->getValue('title');
+                $this->albums->addAlbum($artist, $title);
+
+                // Redirect to list of albums
+                $url = $this->router->assemble(
+                    array('controller' => 'album', 'action' => 'index'),
+                    array('name' => 'default')
+                );
+                $this->response->setStatusCode(302);
+                $this->response->headers()->addHeaderLine('Location', $url);
+                return $this->response;
+            } else {
+                $form->populate($formData);
+            }
+        }
+
+        return array('form' => $form);
     }
 
     public function setTable(Albums $table)
